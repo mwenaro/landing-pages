f5794444d1d761919eda36d348aadc0ce0c30e6a
Mwero Abdalla <mwerothewebmaker@gmail.com>
2023-08-17 20:40:32 +0300

    chore : add yup to the form

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,46 +1,80 @@
-"use client"
-
 import React from 'react';
-import { Formik, Form } from 'formik';
-import Input from './Input'; // Assuming the Input component is in a separate file
-import { Button } from "@/components"
+import { Formik, Form, FormikHelpers } from 'formik';
+import * as Yup from 'yup';
+import Input from './Input';
+import { Button } from "@/components";
 
-const ContactForm = () => {
-  const initialValues = {
+interface ContactFormValues {
+  fullName: string;
+  email: string;
+  phone: string;
+  message: string;
+  subject: string;
+}
+
+const ContactForm: React.FC = () => {
+  const initialValues: ContactFormValues = {
     fullName: '',
     email: '',
     phone: '',
     message: '',
-    subject: ""
+    subject: ''
   };
 
-  const handleSubmit = (values: any) => {
+  const validationSchema = Yup.object().shape({
+    fullName: Yup.string().required('Full Name is required'),
+    email: Yup.string().email('Invalid email address').required('Email is required'),
+    phone: Yup.string().required('Phone is required'),
+    subject: Yup.string().required('Subject is required'),
+    message: Yup.string().required('Message is required')
+  });
+
+  const handleSubmit = (
+    values: ContactFormValues,
+    { setSubmitting }: FormikHelpers<ContactFormValues>
+  ) => {
     // Handle form submission here
     console.log(values);
+    // After handling submission, you can reset form state
+    setSubmitting(false);
   };
 
   return (
-    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-      <Form className="w-full flex flex-col gap-3 bg-slate-300 max-w-[500px] mx-auto py-4 md:py-8">
-        <Input name="fullName" label="Full Name" />
-        <Input name="email" label="Email" type="email" />
-        <Input name="phone" label="Phone" type="tel" />
-        <Input name="subject" label="Subject" type="select" options={[
-          "",
-          "Paternity / DNA Services",
-          "Cancer Testing Services",
-          "Life-Geno (Predictive DNA Services)",
-          "Pharmacogenomics"
-        ]} />
-        <Input name="message" label="Message" type="textarea" />
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+      validationSchema={validationSchema}
+    >
+      {({ isSubmitting }) => (
+        <Form className="w-full flex flex-col gap-3 bg-slate-300 max-w-[500px] mx-auto py-4 md:py-8">
+          <Input name="fullName" label="Full Name" />
+          <Input name="email" label="Email" type="email" />
+          <Input name="phone" label="Phone" type="tel" />
+          <Input
+            name="subject"
+            label="Subject"
+            type="select"
+            options={[
+              '',
+              'Paternity / DNA Services',
+              'Cancer Testing Services',
+              'Life-Geno (Predictive DNA Services)',
+              'Pharmacogenomics'
+            ]}
+          />
+          <Input name="message" label="Message" type="textarea" />
 
-
-
-        <button className="mx-auto w-5/6 md:w-[400px] px-8 py-3 bg-blue-600 text-white my-4 rounded-lg ">Submit</button>
-      </Form>
+          <button
+            type="submit"
+            disabled={isSubmitting}
+            className="mx-auto w-5/6 md:w-[400px] px-8 py-3 bg-blue-600 text-white my-4 rounded-lg"
+          >
+            Submit
+          </button>
+        </Form>
+      )}
     </Formik>
   );
 };
 
 export default ContactForm;
-
